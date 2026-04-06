71f9a794b76d4907c7fadfd389d5e428e5083b72
Mohamed Nasr <mohamednasrmahmood@gmail.com>
2026-04-06 08:57:40 +0200

    Update Frontend Sections: Update section to get data from Database

--- resources/js/pages/home.jsx
+++ resources/js/pages/home.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import { Link } from '@inertiajs/react';
+import Header from '@/components/shared/header';
+import Footer from '@/components/shared/footer';
+import Hero from '../components/shared/hero';
+import Projects from '../components/shared/projects';
+import WorkExperience from '../components/shared/work-experience';
+import About from '../components/shared/about';
+import Contact from '../components/shared/contact';
 import AppLayout from '@/layouts/app-layout';
-export default function Home() {
+export default function Home({ content }) {
+    let hero_data = content.hero_section;
+
     return (
-        <div className="flex h-screen items-center justify-center">
-            <h1 className="text-4xl font-bold">Welcome to the Home Page</h1>
-        </div>
+        <>
+            <Header />
+            <main>
+                <Hero hero_data={hero_data} />
+                <Projects />
+                <WorkExperience />
+                <About />
+                <Contact />
+            </main>
+
+            <Footer />
+        </>
     );
 }
 
